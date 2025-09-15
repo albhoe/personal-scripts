e83bcb283336d4a3787c512bb15fd35aae3d1e24
Albert Hoe <albertjameshoe@gmail.com>
2025-09-14 23:19:57 -0400

    randomSelector written

--- randomSelector.py
+++ randomSelector.py
@@ -1,13 +1,27 @@
 import os
+import cv2
 from random import randrange
 
 root_dir = "C:\\Users\\alber\\Desktop\\D&DCharacters\\Reference Images\\why"
+destination_dir = "C:\\Users\\alber\\Desktop\\D&DCharacters\\Reference Images\\why\\usable"
 
+usable = False
 files = []
 for f in os.listdir(root_dir):
-    f = os.path.join(root_dir,f)
-    if os.path.isfile(f) and not f.startswith('.') and not f.__eq__(__file__):
+    file = os.path.join(root_dir,f)
+    if os.path.isfile(file) and not file.startswith('.') and not file.__eq__(__file__):
         files.append(f)
-pick = files[randrange(0,len(files))]
-print(pick)
-b = os.open(pick,os.O_RDONLY)
+
+while not usable:
+    
+    pick = files[randrange(0,len(files))]
+
+    img = cv2.imread(os.path.join(root_dir,pick), cv2.IMREAD_COLOR)
+    cv2.imshow(pick, img)
+    cv2.waitKey(0)
+    i = input("Usable? (y/n):")
+    if i.lower() == 'y':
+        usable = True
+        os.rename(os.path.join(root_dir,pick), os.path.join(destination_dir,pick))
+        os.remove(os.path.join(root_dir,pick))
+    cv2.destroyAllWindows()
