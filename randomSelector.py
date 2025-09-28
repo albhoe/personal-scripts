ef9c9ed859947dc505c831e8bc29fe19d042846e
Albert Hoe <albertjameshoe@gmail.com>
2025-09-28 10:54:10 -0400

    gitignore

--- randomSelector.py
+++ randomSelector.py
@@ -1,3 +1,5 @@
+#python -u C:\Users\alber\Desktop\scripts\randomSelector.py "C:\\Users\\alber\\Desktop\\D&DCharacters\\Reference Images\\why" 
+
 import os
 import cv2
 from random import randrange
@@ -6,23 +8,25 @@ import sys
 if len(sys.argv) > 1:
     root_dir = sys.argv[1]
 if len(sys.argv) > 2:
-    keyword = sys.argv[2]
+    search = sys.argv[2]
 else:
     root_dir = "C:\\Users\\alber\\Desktop\\D&DCharacters\\Reference Images\\why"
-
+    search = ''
 destination_dir = os.path.join(root_dir,"usable")
-
 usable = False
-screen_x,screen_y = (1000,600)
+screen_x,screen_y = (1500,700)
 
 reportPath = os.path.join(root_dir,".most_recent.txt")
+
 with open(reportPath,"w") as rf:
     rf.write ("File name: Usability\n")
     while not usable:
         dir = os.listdir(root_dir)
         pick = dir[randrange(0,len(dir))]
         file = os.path.join(root_dir,pick)
-        if os.path.isfile(file) and not file.startswith('.') and not file.__eq__(__file__) and (file.endswith('.jpg') or file.endswith('.png') or file.endswith('.jpeg')):
+        if os.path.isfile(file) and not file.__eq__(__file__) and \
+            (file.endswith('.jpg') or file.endswith('.png') or file.endswith('.jpeg')) \
+                and pick[:pick.rfind('.')].find(search) >= 0:
             print (pick)
             img = cv2.imread(os.path.join(root_dir,pick), cv2.IMREAD_COLOR)
             sizey,sizex=img.shape[0],img.shape[1]
@@ -32,9 +36,9 @@ with open(reportPath,"w") as rf:
             if sizey > screen_y:
                 sizex = sizex * screen_y / sizey
                 sizey = screen_y
-            print(sizex,sizey)
             img = cv2.resize(img, (int(sizex), int(sizey)))
             cv2.imshow(pick, img)
+            cv2.moveWindow(pick, 0,0)
             cv2.waitKey(0)
             cv2.destroyAllWindows()
             i = input("Usable? (y/n/q):")
